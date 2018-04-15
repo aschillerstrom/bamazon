646b6ac410a090668a0048ab7d2b290b49b5a406
Amy Schillerstrom <amyschillerstrom@amys-mbp.attlocal.net>
2018-04-15 18:23:39 -0500

    good through purchase...need to fix inventory update.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,6 +15,7 @@ var connection = mysql.createConnection({
 
 //display inventory
 function displayItems(){
+    console.log ("display"); //make sure in display function
     queryInventory = 'SELECT * FROM products';
 
     connection.query(queryInventory, function(err,data){
@@ -26,9 +27,9 @@ function displayItems(){
         var inventory = ' ';
         for (var i=0; i<data.length; i++){
             inventory = ' ';
-            inventory += 'Item ID: '+ data[i].item_id + ' -- ';
+            inventory += 'Item ID Number: '+ data[i].item_id + ' -- ';
             inventory += 'Product Name: ' + data[i].product_name + ' -- ';
-            inventory += 'Proce:  ' + data[i].price + ' -- \n';
+            inventory += 'Price:  ' + data[i].price + ' -- \n';
 
             console.log (inventory);
 
@@ -39,9 +40,71 @@ function displayItems(){
         purchase();
     })
 
-};
+}
 
 //get purchase from user
 
-function purchase()
+
+function purchase() {
+
+    console.log("get user imput"); //make sure in purchase function
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Please enter the Item ID Number of the item you would like to purchase.',
+            filter: Number
+        },
+
+        {
+            type: 'input',
+            name: 'quantity',
+            message: "How many would you like to purchase?",
+            filter: Number
+        }
+    ]).then (function(input) {
+        console.log (input.item_id + "  " + input.quantity); //see if we can see what the user wants to purchase
+
+        var item = input.item_id;
+        var quantity = input.quantity;
+
+        var inventory = 'SELECT * FROM products WHERE ?';           //does the item exist and is there enough of item?
+
+        connection.query(inventory, {item_id:item}, function(err, data) {
+            if (err) throw err;
+
+            if( data.length === 0){
+                console.log("Please input a valid Item ID Number");
+                inventory();
+            }
+            else {
+                var itemInfo = data[0];
+
+                if (quantity <= itemInfo.stock_quantity) {
+                    console.log ("Thank you for your purchase")
+
+                    var updateInventory = 'UPDATE products SET stock_quantity: ' + (itemInfo.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+                    connection.query(updateInventory, function(err, data){
+                        if (err) throw err;
+                        console.log("Thank you for your order.  Your total is $" + (itemInfo.price*quantity) +".");
+
+                        connection.end();
+                    })
+                }
+                else{
+                    console.log ("Sorry, we cannot fill your order at this time due to low inventory.");
+                }
+            }
+        })
+
+    })
+
+}
+
+function run(){
+    displayItems();
+}
+
+run();
 
